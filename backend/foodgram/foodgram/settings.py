16869c3466805ef577dceb5930e17800209ae0aa
t1m4k <102309776+t1m4k@users.noreply.github.com>
2023-05-18 17:01:44 +0300

    Update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default="1tokpdf*!dyd_4ovo1v+tmm744r9vdld!x*!(d*#pwm2")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
